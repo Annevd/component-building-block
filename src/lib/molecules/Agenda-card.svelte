810be1e3d63484a78a34e9e23935ca89d1456ba7
Anne <144004647+Annevd@users.noreply.github.com>
2024-11-21 13:24:45 +0100

    Update Agenda-card.svelte

--- src/lib/molecules/Agenda-card.svelte
+++ src/lib/molecules/Agenda-card.svelte
@@ -6,10 +6,10 @@
         <div class="info-container">
             <h2>Stekjes maken</h2>
             <ul>
-                <time datetime="13:00">13:00</time>
-                <time datetime="2024-11-03">03/11/2024</time>
-                <address>OBA Linnaeusstraat</address>
-                <p>€10,-</p>
+                <li><time datetime="13:00">13:00</time></li>
+                <li><time datetime="2024-11-03">03/11/2024</time></li>
+                <li><address>OBA Linnaeusstraat</address></li>
+                <li>€10,-</li>
             </ul>
             <a href="/">Meld je aan <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 44 47"><path d="M40.492 18.788 8.942 1.034C4.944-1.218 0 1.673 0 6.262v34.704c0 4.537 4.842 7.433 8.84 5.285l31.549-16.948c4.167-2.239 4.225-8.195.103-10.515Z" fill="#fff"></path></svg></span></a>
         </div>
